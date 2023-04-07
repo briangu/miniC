3bbae748e4ce44bc05fb716d138f6921cdc8c92c
Brian Guarraci <brian.guarraci@gmail.com>
2023-04-07 15:33:07 +0000

    time jit operations

--- jit.c
+++ jit.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <time.h>
 #include "miniC.h"
 
 typedef F (*JF)(F, F);
+typedef clock_t CT;
+
 JF a(size_t s) { R(JF)mmap(0, s, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); }
 V f(JF j, size_t s) { munmap(j, s); }
 ZC G op[256] = { ['+'] = 0x58, ['-'] = 0x5C, ['*'] = 0x59, ['/'] = 0x5E };
@@ -17,6 +20,11 @@ M {
     JF a = g('+'), s = g('-'), m = g('*'), d = g('/');
     F x = 5, y = 3;
     O("Add: %f\nSubtract: %f\nMultiply: %f\nDivide: %f\n", a(x, y), s(x, y), m(x, y), d(x, y));
+    
+    CT st = clock(); L (I i=0; i < 1000000000; i++) a(x,y); CT et = clock();
+    F tt = (F)(et - st) / CLOCKS_PER_SEC;
+    O("Time taken for 10**9 iterations: %f seconds, %.15fs / iter\n", tt, tt/1000000000.0);
+
     f(a, 4096); f(s, 4096); f(m, 4096); f(d, 4096);
     R 0;
 }
