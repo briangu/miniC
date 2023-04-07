74f077c6a578f38956fd10dab316edeea7f2f908
Brian Guarraci <brian.guarraci@gmail.com>
2023-04-07 02:25:34 +0000

    clean up jit

--- jit.c
+++ jit.c
@@ -4,72 +4,26 @@
 #include <sys/mman.h>
 #include "miniC.h"
 
-typedef F (*JitFunc)(F, F);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include "miniC.h"
 
-// Utility function to allocate executable memory
-JitFunc allocate_executable_memory(size_t size) {
-    return (JitFunc)mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+typedef F (*JF)(F, F);
+JF a(size_t s) { return (JF)mmap(0, s, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); }
+V f(JF j, size_t s) { munmap(j, s); }
+JF g(char o) {
+    JF j = a(4096); if (j == MAP_FAILED) return 0;
+    G c[] = {0xf2, 0x0f, 0x00, 0xc1, 0xc3};
+    switch (o) { case '+': c[2] = 0x58; break; case '-': c[2] = 0x5c; break; case '*': c[2] = 0x59; break; case '/': c[2] = 0x5e; break; default: f(j, 4096); return 0; }
+    memcpy(j, c, sizeof(c)); return j;
 }
-
-// Utility function to free executable memory
-void free_executable_memory(JitFunc jit_func, size_t size) {
-    munmap(jit_func, size);
-}
-
-// Generate machine code for arithmetic operations
-JitFunc generate_arithmetic_function(char operation) {
-    JitFunc jit_func = allocate_executable_memory(4096);
-    NP(jit_func != MAP_FAILED);
-
-    // Generate x86-64 instructions for arithmetic operations
-    G fn[] = {
-        0xf2, 0x0f, 0x00, 0xc1, // <op>ss xmm0, xmm1
-        0xc3                    // ret
-    };
-
-    G operation_code;
-    switch (operation) {
-        case '+':
-            operation_code = 0x58;  // addss xmm0, xmm1
-            break;
-        case '-':
-            operation_code = 0x5c;  // subss xmm0, xmm1
-            break;
-        case '*':
-            operation_code = 0x59;  // mulss xmm0, xmm1
-            break;
-        case '/':
-            operation_code = 0x5e;  // divss xmm0, xmm1
-            break;
-        default:
-            free_executable_memory(jit_func, 4096);
-            R NULL;
-    }
-
-    memcpy(jit_func, fn, sizeof(fn));
-    ((G*)jit_func)[2] = operation_code;
-
-    return jit_func;
-}
-
-
 M {
-    JitFunc add_func = generate_arithmetic_function('+');
-    JitFunc sub_func = generate_arithmetic_function('-');
-    JitFunc mul_func = generate_arithmetic_function('*');
-    JitFunc div_func = generate_arithmetic_function('/');
-    
-    F a = 5, b = 3;
-    O("Add: %f\n", add_func(a, b));
-    O("Subtract: %f\n", sub_func(a, b));
-    O("Multiply: %f\n", mul_func(a, b));
-    O("Divide: %f\n", div_func(a, b));
-    
-    free_executable_memory(add_func, 4096);
-    free_executable_memory(sub_func, 4096);
-    free_executable_memory(mul_func, 4096);
-    free_executable_memory(div_func, 4096);
-
+    JF a = g('+'), s = g('-'), m = g('*'), d = g('/');
+    F x = 5, y = 3;
+    O("Add: %f\nSubtract: %f\nMultiply: %f\nDivide: %f\n", a(x, y), s(x, y), m(x, y), d(x, y));
+    f(a, 4096); f(s, 4096); f(m, 4096); f(d, 4096);
     R 0;
 }
 
